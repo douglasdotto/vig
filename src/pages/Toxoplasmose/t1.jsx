3c2d7ed5191f15240062ed518586bb22a1961811
Victor Oliveira Sanmartin <victor.sanmartin@tekann.com.br>
2023-02-14 13:14:41 -0300

    take off goback on T1 errors

--- src/pages/Toxoplasmose/t1.jsx
+++ src/pages/Toxoplasmose/t1.jsx
@@ -200,7 +200,7 @@ function T1() {
           >
             <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: -16, marginBottom: 16, }}>
               <Text>Você marcou as opções incorretas, tente novamente</Text>
-              <TouchableOpacity style={{ borderRadius: 15, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 8, paddingVertical: 8, alignSelf: 'stretch', backgroundColor: '#C3272B', marginTop: 16, minWidth: '50%', paddingHorizontal: 16, }}onPress={() => navigation.replace("Toxoplasmose")}>
+              <TouchableOpacity style={{ borderRadius: 15, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 8, paddingVertical: 8, alignSelf: 'stretch', backgroundColor: '#C3272B', marginTop: 16, minWidth: '50%', paddingHorizontal: 16, }}>
                 <Text style={{ color: '#FFFFFF' }}>OK</Text>
               </TouchableOpacity>
             </View>
